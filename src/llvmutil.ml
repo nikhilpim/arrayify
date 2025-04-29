c5ebc1d3c85ce3622decf4e252b0fb45fba657bd
Nikhil Pimpalkhare <nikhil.pimpalkhare@princeton.edu>
2025-04-29 11:41:19 -0400

    duplicates phi nodes

--- src/llvmutil.ml
+++ src/llvmutil.ml
@@ -12,11 +12,13 @@ let llvm_block_to_string (block : llbasicblock) : string =
     String.equal b1_str b2_str
 
 module LlvmNode = struct 
-    type t = int 
-    let compare = Int.compare
-    let equal = Int.equal
+  (* [fst t] is the LLVM label. [snd t] distinguishes between duplicated phi nodes, and should be 0 if it is not a phi node. *)
+    type t = int * int 
+    let compare (l, p) (l', p') = if Int.compare l l' != 0 then Int.compare l l' else Int.compare p p'
+    let equal (l, p) (l', p')  = Int.equal l l' && Int.equal p p'
     let hash a = Hashtbl.hash a
-    let name t = if t = (-1) then "negone" else string_of_int t
+    let name (l, p) = let l_str = if (l = (-1)) then "ret" else string_of_int l in 
+      if p = 0 then l_str else "phi"^l_str^"_"^(string_of_int p)
 end
 
 module LlvmEdge  = struct
@@ -43,6 +45,29 @@ let generate_llvm_ir c_file =
   let mem_buffer = MemoryBuffer.of_file llvm_ir_file_optimized in
   parse_ir ctx mem_buffer
 
+(* A phi node is a LlvmNode where some outgoing edge has a phi instruction in it. 
+LIMITATION: we currently only handle phi nodes which have only one outgoing edge and which only contain one phi instruction. 
+This transformation duplicates the phi node, replaces the phi instruction with the relevant set, and fixes the predecessor edges.
+*)
+let duplicate_phi_nodes (g : LlvmGraph.t) : LlvmGraph.t = 
+  let phi_nodes = LlvmGraph.fold_vertex (fun v acc ->
+      let successor_count = LlvmGraph.out_degree g v in  
+      let is_phi = LlvmGraph.fold_succ_e (fun (_, w, _) acc -> 
+          fold_left_instrs (fun acc instr -> 
+            match instr_opcode instr with 
+            | Opcode.PHI -> (if acc then raise (Failure "Multiple phi instructions in a single block") else if successor_count > 1 then raise (Failure "phi node with multiple successors") else true) 
+            | _ -> acc) acc w
+        ) g v false in
+      if is_phi then v :: acc else acc
+    ) g [] in 
+    List.fold_left (fun g phi_node -> 
+        let with_new_edges, _ = LlvmGraph.fold_pred_e (fun (pred, w, _) (g, index) -> 
+            let new_node = (fst phi_node, index) in
+            LlvmGraph.add_edge_e g (pred, w, new_node), index + 1
+          ) g phi_node (g, 1) in
+        LlvmGraph.remove_vertex with_new_edges phi_node 
+      ) g phi_nodes
+
 let generate_llvm_graph_from_ir m = 
   (* For now, we require that the input is a single function. *)
   let func = fold_left_functions (fun acc func -> 
@@ -65,23 +90,23 @@ let generate_llvm_graph_from_ir m =
     ) [] func in 
   (* The entry is the first block of this function that the LLVM module returns *)
   let all_blocks = fold_left_blocks (fun acc block -> block :: acc) [] func 
-    |> List.mapi (fun i e -> (i, e)) in
+    |> List.mapi (fun i e -> ((i, 0), e)) in
   let g = List.fold_left (fun graph (i, _) -> LlvmGraph.add_vertex graph i) LlvmGraph.empty all_blocks in
   (* -1 is the designated end vertex. *)
-  let g = LlvmGraph.add_vertex g (-1) in
+  let g = LlvmGraph.add_vertex g (-1, 0) in
   let g = List.fold_left (fun graph (i, block) -> 
       let terminator = block_terminator block in
       match terminator with
       | None -> graph
       | Some term -> 
         (* If we're looking at a return, we want to add an edge to a designated end location (-1). I assume that returns are all terminators with no successors. *)
-        if num_successors term = 0 then (LlvmGraph.add_edge_e graph (i, block, -1)) else 
+        if num_successors term = 0 then (LlvmGraph.add_edge_e graph (i, block, (-1, 0))) else 
           (fold_successors (fun succ graph -> 
             let (i', _) = List.find (fun (_, block') -> block_eq block' succ) all_blocks in
             LlvmGraph.add_edge_e graph (i, block, i')
           ) term graph)
     ) g all_blocks in
-    g, (List.length all_blocks - 1), params
+    g |> duplicate_phi_nodes, (List.length all_blocks - 1, 0), params
 
 let generate_llvm_graph c_file = generate_llvm_graph_from_ir (generate_llvm_ir c_file)
 
